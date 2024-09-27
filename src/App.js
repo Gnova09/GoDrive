52a2f741ab5f50b8e6eefd3c8560c5f62b9d3cde
Georges Bueno <georges.bueno@hotmail.com>
2024-09-26 22:20:52 -0400

    Add: se agrego el registro

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/Login/Login';
+import { Home } from './pages/home/Home';
+import { Register } from './pages/Register/Register';
 
 function App() {
   return (
+
     <Router>
 
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<div></div>} />
-      <Route path="/contact" element={<div></div>} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ToastSuccess } from './components/toast/toast';
+import { AppContextProvider } from './context/context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+      <ToastSuccess />
+    </AppContextProvider>
   </React.StrictMode>
 );
 

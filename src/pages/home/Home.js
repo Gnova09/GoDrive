a39a00dd0edc14e5be344a5e5e20d523a7f7db02
georges <georges.bueno@hotmail.com>
2024-10-06 20:38:12 -0400

    Add: home page & estructura de details

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,78 @@
 import React from 'react'
-import Navbar from '../../components/nav/navbar'
+import CardOfCars from '../../components/home/cardOfCars'
+import Features from '../../components/home/feature'
+
 
 export const Home = () => {
 
-    
+  const cars= [
+    { 
+      nombre: 'toyota',
+      precio: 1812132,
+      img: "/assets/carsImgenes/wrangle.jpg"
+    },
+    { 
+      nombre: 'toyota',
+      precio: 1812132,
+      img: "/assets/carsImgenes/wrangle.jpg"
+    },
+    { 
+      nombre: 'toyota',
+      precio: 1812132,
+      img: "/assets/carsImgenes/wrangle.jpg"
+    },
+    { 
+      nombre: 'toyota',
+      precio: 1812132,
+      img: "/assets/carsImgenes/wrangle.jpg"
+    },
+    { 
+      nombre: 'toyota',
+      precio: 1812132,
+      img: "/assets/carsImgenes/wrangle.jpg"
+    },
+    { 
+      nombre: 'toyota',
+      precio: 1812132,
+      img: "/assets/carsImgenes/wrangle.jpg"
+    },
+    { 
+      nombre: 'toyota',
+      precio: 1812132,
+      img: "/assets/carsImgenes/wrangle.jpg"
+    },
+    { 
+      nombre: 'toyota',
+      precio: 1812132,
+      img: "/assets/carsImgenes/wrangle.jpg"
+    },
+    { 
+      nombre: 'toyota',
+      precio: 1812132,
+      img: "/assets/carsImgenes/wrangle.jpg"
+    },
+  ]
+
   return (
-    <div className='w-full m-0 p-0'>
-      home      
+    <div className='  border border-gray-400 mx-20  p-0'>
+
+      <div className='flex h-full  justify-center items-center'>
+      {/* Imagen de portada  */}
+        <img src="/assets/carsImgenes/wrangler2.jpg" className='w-full'  alt="imagen general de un vehiculo" />
+      </div>
+
+      <div class="grid items-center justify-center grid-cols-3 gap-4 pt-2">
+        {
+          cars.map(({nombre,precio,img})=>{
+            return <CardOfCars nombre={nombre} precio={precio} img={img} />
+          })
+        }
+        
+      </div>
+
+      <Features />
+
+      
     </div>
   )
 }

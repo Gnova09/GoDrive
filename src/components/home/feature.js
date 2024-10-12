ab99b24d678e3cb79753a22ba7ade5bbcd40860d
Soybrayangit <fernando232521@gmail.com>
2024-10-12 17:01:45 -0400

    iconos arreglados y cambios en el home

--- src/components/home/feature.js
+++ src/components/home/feature.js
@@ -10,42 +10,42 @@ const Features = () => {
       <div className="grid grid-cols-3 gap-6 text-center">
         {/* Primer ítem */}
         <div className="flex flex-col items-center">
-          <img src="/public/assets/carsImgenes/Seguridad.webp" alt="Seguridad" className="w-12 h-12 mb-2"/>
+          <img src="/assets/carsImgenes/seguridad.png" alt="Seguridad" className="w-12 h-12 mb-2"/>
           <h3 className="text-blue-600 font-bold">SEGURIDAD</h3>
           <p>Sus datos financieros no se comparten con el vendedor.</p>
         </div>
 
         {/* Segundo ítem */}
         <div className="flex flex-col items-center">
-          <img src="/public/assets/carsImgenes/Proteccion.webp" alt="Protección" className="w-12 h-12 mb-2" />
+          <img src="/assets/carsImgenes/proteccion.png" alt="Protección" className="w-12 h-12 mb-2" />
           <h3 className="text-blue-600 font-bold">PROTECCIÓN</h3>
           <p>Sus pagos están protegidos por el programa de protección del comprador.</p>
         </div>
 
         {/* Tercer ítem */}
         <div className="flex flex-col items-center">
-          <img src="/public/assets/carsImgenes/Rapidez.webp" alt="Rapidez" className="w-12 h-12 mb-2" />
+          <img src="/assets/carsImgenes/rapidez.png" alt="Rapidez" className="w-12 h-12 mb-2" />
           <h3 className="text-blue-600 font-bold">RAPIDEZ</h3>
           <p>Recibimos y verificamos su pago instantáneamente.</p>
         </div>
 
         {/* Cuarto ítem */}
         <div className="flex flex-col items-center">
-          <img src="/public/assets/carsImgenes/Flexibilidad.webp" alt="Flexibilidad" className="w-12 h-12 mb-2" />
+          <img src="/assets/carsImgenes/flexibilidad.png" alt="Flexibilidad" className="w-12 h-12 mb-2" />
           <h3 className="text-blue-600 font-bold">FLEXIBILIDAD</h3>
           <p>Usted elige cómo pagar: tarjeta, cuenta bancaria o saldo de Paypal.</p>
         </div>
 
         {/* Quinto ítem */}
         <div className="flex flex-col items-center">
-          <img src="/public/assets/carsImgenes/Comodidad.webp" alt="Comodidad" className="w-12 h-12 mb-2" />
+          <img src="/assets/carsImgenes/comodidad.png" alt="Comodidad" className="w-12 h-12 mb-2" />
           <h3 className="text-blue-600 font-bold">COMODIDAD</h3>
           <p>Pague en 2 clics. No tendrá que introducir sus datos en cada compra.</p>
         </div>
 
         {/* Sexto ítem */}
         <div className="flex flex-col items-center">
-          <img src="/public/assets/carsImgenes/Multimonedas.webp" alt="Multimonedas" className="w-12 h-12 mb-2" />
+          <img src="/assets/carsImgenes/multimonedas.png" alt="Multimonedas" className="w-12 h-12 mb-2" />
           <h3 className="text-blue-600 font-bold">MULTIMONEDAS</h3>
           <p>Paypal acepta más de 24 monedas de circulación mundial.</p>
         </div>

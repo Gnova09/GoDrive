c9f60301f31d314845a116faffa005256c483f78
Georges Bueno <georges.bueno@hotmail.com>
2024-12-05 00:32:20 -0400

    fix

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -100,14 +100,14 @@ const Navbar = () => {
 
             {!islogin ? (
               <>
-                <li>
+                {/* <li>
                   <a
                     href="/register"
                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
                   >
                     Registrar
                   </a>
-                </li>
+                </li> */}
                 <li>
                   <a
                     href="/login"
